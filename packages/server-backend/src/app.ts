3bc58a0a382bfb62cfc43009e44f1b6b49017349
coderodent-calfee <robert.w.calfee@gmail.com>
2024-12-26 10:35:50 -0800

    removed environment files

--- packages/server-backend/src/app.ts
+++ packages/server-backend/src/app.ts
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use('/api', routes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Application Server running on http://localhost:${PORT}`);
 });
 
 app.use((req, res) => {
